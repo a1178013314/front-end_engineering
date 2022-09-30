435a2df9e90ca129a733fa2d946ef4a6085b3db3
a1178013314 <1178013314@qq.com>
2022-09-30 08:57:22 +0800

    feat: 添加git 脚本

--- git/index.js
+++ git/index.js
@@ -1,1 +1,36 @@
-console.log(`当前文件路径是:${__dirname}`)
+let fs = require('fs')
+let path = require('path')
+// console.log(`当前文件路径是:${__dirname}`)
+
+if (__dirname.indexOf("package") !== -1) {
+    copyGitattributes()
+    copyGitignore()
+}else{
+    console.warn("没有在monorepo项目中执行")
+}
+
+function copyGitattributes() {
+    //路径问题不要太纠结先实现基本功能
+    let fromPath = path.join(__dirname, ".gitattributes")
+    let toPaht = path.join(__dirname, "..", "..", "..", ".gitattributes")
+    fs.readFile(fromPath, (err, data) => {
+        fs.writeFile(toPaht, data, (err, deta) => {
+            if (err) {
+                throw (err)
+            }
+        })
+    })
+}
+
+function copyGitignore() {
+    //路径问题不要太纠结先实现基本功能
+    let fromPath = path.join(__dirname, "..gitignore")
+    let toPaht = path.join(__dirname, "..", "..", "..", "..gitignore")
+    fs.readFile(fromPath, (err, data) => {
+        fs.writeFile(toPaht, data, (err, deta) => {
+            if (err) {
+                throw (err)
+            }
+        })
+    })
+}
